ab19e21bf9523f8dea90d646edf7e07173b0263b
claudiawang2017 <892171449@qq.com>
2017-11-24 16:13:20 +0800

    add add_event_test

---
+++ pyrequest/interface/add_event_test.py
@@ -0,0 +1,26 @@
+import  unittest
+import requests
+import os, sys
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parentdir)
+from db_fixture import test_data
+
+
+class AddEventTest(unittest.TestCase):
+    ''' 添加发布会 '''
+
+    def setUp(self):
+        self.base_url = 'http://127.0.0.1:8000/api/add_event/'
+
+    def tearDown(self):
+        print(self.result)
+
+    def test_add_event_all_null(self):
+        ''' 所有参数为空 '''
+        payload = {'eid': '', '': '', 'limit': '', 'address': '', 'start_time': ''}
+        r = requests.post(self.base_url, data=payload)
+        self.result = r.json()
+        self.assertEqual(self.result['status'], 10021)
+        self.assertEqual(self.result['message'], 'parameter errors')
+
+    
